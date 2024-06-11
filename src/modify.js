61bc6c9e87f75fc5a8bb93d6d52fed46337eb55a
kellyxc28 <kellyxiongc@gmail.com>
2024-06-11 19:27:06 -0400

    Completed Question 6: return4RandomColors()

--- src/modify.js
+++ src/modify.js
@@ -4,23 +4,33 @@ const numsToRGBColor = ([color1, color2, color3]) => {
   return `rgb(${color1}, ${color2}, ${color3})`;
 };
 
-const getRandomBytes = () => new Promise((resolve, reject) => {
-  crypto.randomFill(new Uint8Array(3), (err, buffer) => {
-    if (err) return reject(err);
-    resolve([...buffer]);
+const getRandomBytes = () =>
+  new Promise((resolve, reject) => {
+    crypto.randomFill(new Uint8Array(3), (err, buffer) => {
+      if (err) return reject(err);
+      resolve([...buffer]);
+    });
   });
-});
 
 const return4RandomColors = () => {
   const colors = [];
+
   return getRandomBytes()
-    .then(() => {
+    .then((color) => {
+      colors.push(numsToRGBColor(color));
+      return getRandomBytes();
     })
-    .then(() => {
+    .then((color) => {
+      colors.push(numsToRGBColor(color));
+      return getRandomBytes();
     })
-    .then(() => {
+    .then((color) => {
+      colors.push(numsToRGBColor(color));
+      return getRandomBytes();
     })
-    .then(() => {
+    .then((color) => {
+      colors.push(numsToRGBColor(color));
+      return colors;
     })
     .catch((err) => {
       console.error(err);
